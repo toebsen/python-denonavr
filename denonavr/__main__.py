0f8e952b9ddc36a3c665c8d6f1ad66756d5e6e60
toebsen <tobiasbichl@googlemail.com>
2018-12-20 16:07:39 +0100

    - minor changes - add travis and mini unittest

--- denonavr/__main__.py
+++ denonavr/__main__.py
@@ -1,45 +1,44 @@
-import telnetlib
 import argparse
-
-from flask import Flask, jsonify, request, abort
-import yaml
 import logging
+import telnetlib
 
+import yaml
+from flask import Flask, jsonify
 
 CONFIG = {
     "host": "192.168.2.25",
     "port": "23",
     "timeout": 5
-    }
+}
 
 app = Flask(__name__)
 
 
 def _is_valid_input_source(source):
-    """
-    only allow special sources
-    :param source:
+    """Only allow special sources.
+
+    :param source: 
     :return:
     """
-    if source in ["DVD", "BD", "GAME", "SAT/CBL"]:
-        return True
-    return False
+    return source in ["DVD", "BD", "GAME", "SAT/CBL"]
+
 
 def _convert_input_source(source):
+    """Convert input source to proper denon conform name.
+
+    :param source: name of the source
+    :return: source name adapted to protocol
     """
-    convert input source to proper denon conform name
-    :param source:
-    :return:
-    """
+
     source = source.upper()
     if "SATCBL" in source:
         source = "SAT/CBL"
     return source
 
 
-def execute(command, config=CONFIG):
-    """
-    execute telnet commands on DENON host
+def execute(command, config):
+    """Execute telnet commands on DENON host.
+
     :param command: see https://www.denon.de/de/product/homecinema/avreceiver/avrx1200w?docname=Steuerungsprotokoll_IP_RS232C_AVR-X1200W_AVR-X2200W_AVR-X3200W_AVR-X4200W.pdf
     :param config:
     :return:
@@ -47,66 +46,71 @@ def execute(command, config=CONFIG):
     try:
         tn = telnetlib.Telnet(config["host"], config["port"], config["timeout"])
     except Exception as e:
-        logging.error("Exception occured: %r" % e)
+        logging.error("Exception occurred: %r" % e)
         return "ERROR"
 
     try:
         tn.write(("%s\r" % command).encode("ascii"))
-        return str(tn.read_until(("\r").encode('ascii'))).replace("b'", "").replace("\\r'", "")
-    except:
-        logging.error("Exception occured: %r" % e)
-        return "ERROR"
+        return str(tn.read_until("\r".encode('ascii'))).replace("b'", "").replace("\\r'", "")
+    except OSError:
+        logging.exception("Exception occurred during writing of telnet")
     finally:
         tn.close()
+        return "ERROR"
+
 
 @app.route('/power/state', methods=['GET'])
 def get_power_state():
-    """
+    """Get current power state.
+
     :return: current power state PWON/PWStandby
     """
-    return jsonify(power=execute("PW?"))
+    return jsonify(power=execute("PW?", CONFIG))
+
 
 @app.route('/power/turnon', methods=['GET'])
 def turn_on():
+    """Turn on System.
+
+    :return: success
     """
-    Turn on System
-    :return: succes
-    """
-    if execute("PW?") == "PWON":
+    if execute("PW?", CONFIG) == "PWON":
         return jsonify(power="PWON")
     else:
-        return jsonify(power=execute("PWON"))
+        return jsonify(power=execute("PWON", CONFIG))
+
 
 @app.route('/power/turnoff', methods=['GET'])
 def turn_off():
+    """Turn off System.
+
+    :return: success
     """
-    Turn off System
-    :return: succes
-    """
-    if execute("PW?") == "PWSTANDBY":
+    if execute("PW?", CONFIG) == "PWSTANDBY":
         return jsonify(power="PWSTANDBY")
     else:
-        return jsonify(power=execute("PWSTANDBY"))
+        return jsonify(power=execute("PWSTANDBY", CONFIG))
+
 
 @app.route('/input/state', methods=['GET'])
 def get_input_state():
-    """
-    Get current Input Source
+    """ Get current Input Source.
+
     :return: success
     """
-    return jsonify(power=execute("SI?"))
+    return jsonify(power=execute("SI?", CONFIG))
+
 
 @app.route('/input/switch/<source_id>', methods=['GET'])
 def switch_input_source(source_id):
-    """
-    Set new Input Source
+    """Set new Input Source.
+
     :param source_id:
     :return: success
     """
     success = False
-
     source = _convert_input_source(source_id)
-    current_source = execute("SI?").replace("SI", "")
+    current_source = execute("SI?", CONFIG).replace("SI", "")
     print("Requested Source: " + source + " Current SRC: " + current_source)
 
     if source in current_source:
@@ -115,41 +119,44 @@ def switch_input_source(source_id):
     elif not _is_valid_input_source(source):
         success = False
     else:
-        execute("SI" + source)
+        execute("SI" + source, CONFIG)
         success = True
 
     return jsonify(succes=success)
 
+
 @app.route('/volume/level', methods=['GET'])
 def get_volume_level():
-    """
+    """Get the volume level on volume/level.
+
     :return: Get current volume level
     """
-    return jsonify(level=execute("MV?").replace("MV", ""))
+    return jsonify(level=execute("MV?", CONFIG).replace("MV", ""))
+
 
 @app.route('/volume/set/<int:level_id>', methods=['GET'])
 def set_volume_level(level_id):
-    """
-    Set Volume level
+    """Set Volume level on /volume/set/*.
+
     :param level_id: db level in the range of 0-60DB
     :return: success
     """
     success = False
-    if level_id >= 0 and level_id <= 60:
-        execute("MV%02d" % level_id)
+    if 0 <= level_id <= 60:
+        execute("MV%02d" % level_id, CONFIG)
         success = True
     return jsonify(success=success)
 
-def _read_config(config_file_path):
 
-    global CONFIG
+def _read_config(config_file_path):
     """ Parse Config File from yaml file. """
+    global CONFIG
     config_file = open(config_file_path, 'r')
     config = yaml.load(config_file)
     config_file.close()
     print(CONFIG)
     CONFIG = config["denon"]
-    print( CONFIG )
+    print(CONFIG)
 
 
 def main():
@@ -163,6 +170,7 @@ def main():
         _read_config(args.config)
 
     app.run(host='0.0.0.0', port=args.port)
+
 
 if __name__ == '__main__':
     main()
---
+++ test/test__main__,py.py
@@ -0,0 +1,31 @@
+import unittest
+import unittest.mock
+
+import denonavr.__main__ as avr
+
+
+class TestDenonAVR(unittest.TestCase):
+    def test_valid_input_source(self):
+        self.assertTrue(avr._is_valid_input_source("DVD"))
+        self.assertTrue(avr._is_valid_input_source("BD"))
+        self.assertTrue(avr._is_valid_input_source("GAME"))
+        self.assertTrue(avr._is_valid_input_source("SAT/CBL"))
+        self.assertFalse(avr._is_valid_input_source("VHS"))
+
+    def test_convert_input_source(self):
+        self.assertEqual(avr._convert_input_source("satcbl"), "SAT/CBL")
+        self.assertEqual(avr._convert_input_source("vhs"), "VHS")
+
+    def test_execute(self):
+        with unittest.mock.patch("telnetlib.Telnet") as telnet_mock:
+            telnet_mock.side_effect = OSError
+            self.assertEqual(avr.execute("CMD", avr.CONFIG), "ERROR")
+
+            telnet_mock.return_value = unittest.mock.MagicMock(spec=["write", "read_until", "close"])
+            telnet_mock.side_effect = None
+            telnet_mock.return_value.write.side_effect = OSError
+            self.assertEqual(avr.execute("CMD", avr.CONFIG), "ERROR")
+
+
+if __name__ == '__main__':
+    unittest.main()
