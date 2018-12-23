5648d735de443b6c43615b01e92809db30e5e8fa
toebsen <toebsen@hotmail.com>
2018-12-23 12:01:06 +0100

    minor improvements

--- test/test__main__.py
+++ test/test__main__.py
@@ -18,6 +18,13 @@ class TestDenonAVR(unittest.TestCase):
 
     def test_execute(self):
         with unittest.mock.patch("telnetlib.Telnet") as telnet_mock:
+            telnet_mock.return_value.read_until.return_value = "b'Test\\r'"
+            self.assertEqual(avr.execute("?Test", avr.CONFIG), "Test")
+            telnet_mock.return_value.write.assert_called_once_with(b'?Test\r')
+            telnet_mock.return_value.close.assert_called_once()
+
+    def test_execute_error(self):
+        with unittest.mock.patch("telnetlib.Telnet") as telnet_mock:
             telnet_mock.side_effect = OSError
             self.assertEqual(avr.execute("CMD", avr.CONFIG), "ERROR")
 
@@ -25,6 +32,7 @@ class TestDenonAVR(unittest.TestCase):
             telnet_mock.side_effect = None
             telnet_mock.return_value.write.side_effect = OSError
             self.assertEqual(avr.execute("CMD", avr.CONFIG), "ERROR")
+            telnet_mock.return_value.close.assert_called()
 
 
 if __name__ == '__main__':
